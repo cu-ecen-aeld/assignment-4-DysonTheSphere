8556a618ee9da11dc77945b4e0e213f6916fb0c8
DysonTheSphere <disabledfishinabarrel@gmail.com>
2026-09-21 02:40:13 -0600

    Fully functional, autotester was a pain to troubleshoot since I looked everywhere else first. Including perma-trusting the localhost since ssh keys would wipe on distclean

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -19,10 +19,11 @@ define AESD_ASSIGNMENTS_BUILD_CMDS
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
+# Had to fix your path due to autograder mismatch, assignment4-buildroot contains the script for full_test
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
-	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4-buildroot/* $(TARGET_DIR)/usr/bin/
 
 	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin/
 	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin/
